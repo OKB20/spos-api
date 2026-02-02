82d1e2e11096cbe10f24f881212e522fd507d61d
OKB20 <91167710+OKB20@users.noreply.github.com>
2026-02-02 11:34:14 -0600

    ip host change

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 PORT_VALUE="${PORT:-8000}"
-HOST_VALUE="${HOST:-0.0.0.0}"
+HOST_VALUE="${HOST:-66.241.124.70}"
 
 exec /app/.venv/bin/uvicorn app.main:app --host "${HOST_VALUE}" --port "${PORT_VALUE}"
